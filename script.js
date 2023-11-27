3b3591d3229bc1b3e52b96264e657595850f8dac
ManishDubey95 <136079775+ManishDubey95@users.noreply.github.com>
2023-11-27 12:47:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let student = {
+    name: "Manish"
+}
+
+Object.prototype.getKeys = function() {
+    return Object.keys(this);
+}
+
+const keys = student.getKeys();
+console.log(keys);
